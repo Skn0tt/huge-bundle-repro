dda2a34c2b63dbfa67db5fdd50abc9e07752855b
Simon Knott <info@simonknott.de>
2023-04-18 16:12:50 +0200

    reproduce giant sites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en", "fr", "nl", "de", "pt", "es", "it", "ja", "ko", "zh"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
